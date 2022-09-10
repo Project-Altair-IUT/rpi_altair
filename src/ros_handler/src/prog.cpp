ce1dea795a5f67e44b85cd4c301a428ca2407d9c
unknown <rakinuzzaman@gmail.com>
2022-09-10 11:38:22 +0600

    added arm handling

--- src/ros_handler/src/prog.cpp
+++ src/ros_handler/src/prog.cpp
@@ -9,6 +9,7 @@
 #include "std_msgs/Int16.h"
 #include "std_msgs/Int8.h"
 #include <std_msgs/Int16MultiArray.h>
+#include <std_msgs/Int8MultiArray.h>
 #include <geometry_msgs/Twist.h>
 
 #define rc_min 1000
@@ -20,12 +21,21 @@
 #define angular_max 5.5
 
 int16_t ch[8];
+int8_t arm_arr[6];
+std_msgs::Int8MultiArray arm_msg;
 
 float rangeMap(int16_t x, int16_t in_min, int16_t in_max, float out_min, float out_max)
 {
   if(x <= 2000 and x >= 1000 )
   return (1.0*x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   else return 0.0;
+}
+
+int8_t rangeMap(bool f, int16_t x, int16_t in_min, int16_t in_max, int8_t out_min, int8_t out_max)
+{
+  if(x <= 2000 and x >= 1000 )
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+  else return 0;
 }
 
 void rcCallback(const std_msgs::Int16MultiArray& msg)
@@ -45,27 +55,60 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   
   ros::Publisher cmd_vel = n.advertise<geometry_msgs::Twist>("cmd_vel", 100);
+  ros::Publisher arm_cmd = n.advertise<std_msgs::Int8MultiArray>("arm_cmd", 10);
+  ros::Publisher col_cmd = n.advertise<std_msgs::Int8MultiArray>("col_cmd", 10);
+
 
   ros::Rate rate(10);
   
   geometry_msgs::Twist velocity;
 
-  velocity.linear.x = 0.0;
-  velocity.linear.y = 0.0;
-  velocity.linear.z = 0.0;
-  velocity.angular.x = 0.0;
-  velocity.angular.y = 0.0;
-  velocity.angular.z = 0.0;
   
+  
+
   
   ros::Subscriber sub = n.subscribe("rc_signal", 100, rcCallback);
 
   
   while(ros::ok()){
 
-    velocity.linear.x = rangeMap(ch[2], rc_min, rc_max, linear_min, linear_max);
-    velocity.angular.z = rangeMap(ch[3], rc_min, rc_max, angular_min, angular_max);
+    velocity.linear.x = 0.0;
+    velocity.linear.y = 0.0;
+    velocity.linear.z = 0.0;
+    velocity.angular.x = 0.0;
+    velocity.angular.y = 0.0;
+    velocity.angular.z = 0.0;
 
+    for(int i=0; i<6; i++)
+    {
+      arm_msg.data[i] = 0;
+    }
+
+    if(ch[4]>=1000 and ch[4] <= 1200)
+    {
+      //rover drive
+      velocity.linear.x = rangeMap(ch[2], rc_min, rc_max, linear_min, linear_max);
+      velocity.angular.z = rangeMap(ch[3], rc_min, rc_max, angular_min, angular_max);
+    }
+    else if(ch[4]>=1300 and ch[4] <= 1700)
+    {
+      arm_msg.data[0] = rangeMap(1, ch[0], rc_min, rc_max, 0, 255); //base
+      arm_msg.data[1] = rangeMap(1, ch[2], rc_min, rc_max, 0, 255); //primary
+      arm_msg.data[2] = rangeMap(1, ch[1], rc_min, rc_max, 0, 255); //secndary
+      arm_msg.data[3] = rangeMap(1, ch[5], rc_min, rc_max, 0, 255); //wrist pitch
+
+      arm_msg.data[4] = rangeMap(1, ch[3], rc_min, rc_max, 0, 180); //wrist roll servo
+      arm_msg.data[5] = rangeMap(1, ch[6], rc_min, rc_max, 0, 180); //gripper sesrvo
+
+      // arm_msg.data_length = 6;
+    }
+    else if(ch[4] >= 1750 and ch[4] <= 2000)
+    {
+
+    }
+
+    arm_cmd.publish(arm_msg);
+    
     cmd_vel.publish(velocity);
      
     rate.sleep();
